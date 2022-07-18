3058a3a3f6029d35fe3e5f4fca7e24e80399085f
Mia Borja <apk.borja@gmail.com>
2022-07-17 22:40:13 -0700

    updated App components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
-import logo from './logo.svg';
+import React from 'react';
+import Header from './components/Header';
+import SiteCon from './components/SiteCon';
+import Footer from './components/Footer';
 import './App.css';
 
 function App() {
---
+++ src/components/SiteCon.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Nav from './Nav';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Portfolio from './pages/Portfolio';
+import Resume from './pages/Resume';
+
+function SiteCon() {
+    return (
+        <Nav />
+    );
+}
+
+export default SiteCon;
